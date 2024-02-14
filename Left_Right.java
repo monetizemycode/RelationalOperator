82c31577e27d3d787f34775222e062158345e333
Avinash <159287552+monetizemycode@users.noreply.github.com>
2024-02-14 21:36:53 +0530

    Add files via upload

---
+++ Left_Right.java
@@ -0,0 +1,12 @@
+class Left_Right {
+public static void main (String[]lr)
+{
+//Checking the condition L.H.S < R.H.s
+int value = 25;
+int value2 = 26;
+boolean A = value < value2;
+
+//Print result
+System.out.println("Checking L.H.S < R.H.S:" + A);
+}
+}
---
+++ VotingAge.java
@@ -0,0 +1,11 @@
+class VotingAge
+{
+public static void main(String[]va)
+{
+int personAge = 17;
+int votingAge = 18;
+boolean A = personAge > votingAge;
+
+System.out.print("voting right: " + A);
+}
+}
